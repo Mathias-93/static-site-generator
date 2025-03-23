9885f09706914dd8c64e8c6cda62399301b985c8
Mathias-93 <iwbsc2@gmail.com>
2025-03-23 13:34:04 +0100

    added texttype class and Enum

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from textnode import TextType
+
+def main():
+    text_node = TextType("This is some anchor text", "link", "https://www.boot.dev")
+    print(text_node)
+
+
+main()
